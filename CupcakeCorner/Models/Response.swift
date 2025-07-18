1c4c1ba26d55d2dd35a67d884f70ce2fcc6f160d
Jorge Encinas <jorgecarlosencinas@gmail.com>
2025-07-18 12:48:14 -0700

    making requessts

---
+++ CupcakeCorner/Models/Response.swift
@@ -0,0 +1,11 @@
+//
+//  Response.swift
+//  CupcakeCorner
+//
+//  Created by Jorge Encinas on 7/18/25.
+//
+import SwiftUI
+
+struct Response : Codable {
+    var results: [Result]
+}
---
+++ CupcakeCorner/Models/Result.swift
@@ -0,0 +1,13 @@
+//
+//  Result.swift
+//  CupcakeCorner
+//
+//  Created by Jorge Encinas on 7/18/25.
+//
+import SwiftUI
+
+struct Result : Codable {
+    var trackId : Int
+    var trackName : String
+    var collectionName : String
+}
