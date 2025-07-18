0b77717efd45d604251862d78e080a95ebbcef7e
Jorge Encinas <jorgecarlosencinas@gmail.com>
2025-07-18 12:11:42 -0700

    First commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Jorge Encinas on 7/18/25.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
