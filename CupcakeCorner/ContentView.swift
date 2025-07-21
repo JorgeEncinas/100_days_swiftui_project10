8fdde95d58aa6cc9ba749b7ff9227a9fe56f1551
Jorge Encinas <jorgecarlosencinas@gmail.com>
2025-07-21 11:59:15 -0700

    Sending and receiving order over the internet

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         //ListTracksView()
-        AsyncImgView3()
+        MainView()
     }
 }
 
--- CupcakeCorner/Views/CheckoutView.swift
+++ CupcakeCorner/Views/CheckoutView.swift
@@ -9,6 +9,46 @@ import SwiftUI
 struct CheckoutView : View {
     var order: Order
     
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    
+    func placeOrder() async {
+        // 1. Convert our `order` into JSON
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        // 2. Tell Swift how to send that data over a net call
+        let url = URL(string: "https://reqres.in/api/cupcakes")
+        guard let urlUnwrapped = url else {
+            print("URL invalid")
+            return
+        }
+        var request = URLRequest(url : urlUnwrapped)
+        //Must force URL, otherwise type is `URL?`
+        
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        // 3. Run that request and process the response.
+        do {
+            let (data, _) = try await URLSession.shared.upload(
+                for: request,
+                from: encoded
+            )
+            let decodedOrder = try JSONDecoder().decode(
+                Order.self,
+                from: data
+            )
+            confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+            showingConfirmation = true
+        } catch {
+            print("Checkout failed : \(error.localizedDescription)")
+        }
+        
+        
+    }
+    
     var body : some View {
         ScrollView {
             VStack {
@@ -29,13 +69,22 @@ struct CheckoutView : View {
                 Text("Your total is \(order.cost, format: .currency(code: "USD"))")
                     .font(.title)
                 
-                Button("Place order", action: { })
-                    .padding()
+                Button("Place order") {
+                    Task {
+                        await placeOrder()
+                    }
+                }
+                .padding()
             }
         }
         .navigationTitle("Check Out")
         .navigationBarTitleDisplayMode(.inline)
         .scrollBounceBehavior(.basedOnSize)
+        .alert("Thank you!", isPresented: $showingConfirmation) {
+            Button("OK") { }
+        } message : {
+            Text(confirmationMessage)
+        }
     }
 }
 
